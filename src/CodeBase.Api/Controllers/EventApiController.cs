5c3bc865c93876fab8f725b7746320b76e870bd8
Quang Phan <oclockvn@gmail.com>
2019-07-06 20:30:57 +0700

    Add event api

---
+++ src/CodeBase.Api/Controllers/EventApiController.cs
@@ -0,0 +1,27 @@
+﻿using CodeBase.Core.Services.Event;
+using CodeBase.Core.ValueObjects;
+using CodeBase.Core.ValueObjects.Events;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace CodeBase.Api.Controllers
+{
+    [RoutePrefix("events")]
+    public class EventApiController : BaseApiController
+    {
+        private readonly IEventService _eventService;
+
+        public EventApiController(IEventService eventService)
+        {
+            _eventService = eventService;
+        }
+
+        [HttpGet, Route("add")]
+        public async Task<ResponseContext<EventResponseData>> AddEventAsync(RequestContext<EventRequestData> request)
+        {
+            var context = await _eventService.AddEventAsync(request.Payload);
+
+            return OkResponse(context);
+        }
+    }
+}
